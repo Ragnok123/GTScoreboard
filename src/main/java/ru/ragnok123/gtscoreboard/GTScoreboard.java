693d835dd42aea571f12cd869b69b8999414a646
Ragnok123 <gebab.urumci.kv@gmail.com>
2023-10-23 12:16:05 +0200

    powernukkit

--- src/main/java/ru/ragnok123/gtscoreboard/GTScoreboard.java
+++ src/main/java/ru/ragnok123/gtscoreboard/GTScoreboard.java
@@ -3,23 +3,13 @@ package ru.ragnok123.gtscoreboard;
 import ru.ragnok123.gtscoreboard.protocol.*;
 import ru.ragnok123.gtscoreboard.scoreboard.*;
 import cn.nukkit.event.Listener;
-import cn.nukkit.event.player.PlayerInteractEvent;
-import cn.nukkit.event.player.PlayerJoinEvent;
 import cn.nukkit.event.player.PlayerQuitEvent;
 import cn.nukkit.event.EventHandler;
-import cn.nukkit.event.server.*;
-import cn.nukkit.network.protocol.*;
 import cn.nukkit.plugin.PluginBase;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import cn.nukkit.Player;
-import cn.nukkit.Server;
-import cn.nukkit.command.Command;
-import cn.nukkit.command.CommandSender;
 public class GTScoreboard extends PluginBase implements Listener{
 	
 	public static HashMap<Player, Scoreboard> boards = new HashMap<Player, Scoreboard>();
